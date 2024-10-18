59384f9f6449f22e6428e2ea4d227eef76200edb
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2024-10-18 07:19:20 +0100

    env

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', userRouter);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to curve-portal-backend server!");
+  res.send(`Welcome to curve-portal-backend server! mode ${process.env.NODE_ENV}`);
 });
 
 // Handle Unhandled ROutes
